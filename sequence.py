d1e91fbf6e9d25d90b982df00bef9c90215e8694
yuanzhep <93398024+yuanzhep@users.noreply.github.com>
2025-08-05 16:11:12 -0400

    Update

--- sequence.py
+++ sequence.py
@@ -3,9 +3,9 @@ import numpy as np
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM 
 
-model_dir = ".../RME/llama" 
-input_path = ".../llama_input/prompt_sequence.npy"
-save_path = ".../prediction/B25_Ant1_f1_S47_pred_tokens.npy"
+model_dir = ".../llama" 
+input_path = ".../prompt_sequence.npy"
+save_path = ".../prediction"
 os.makedirs(os.path.dirname(save_path), exist_ok=True)
 tokenizer = AutoTokenizer.from_pretrained(model_dir, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(
@@ -13,16 +13,14 @@ model = AutoModelForCausalLM.from_pretrained(
     torch_dtype=torch.float32,
     trust_remote_code=True
 ).eval()
-print("Model loaded.")
+print("Model loaded")
 
 full_seq = np.load(input_path)  # shape: [5, H, W]
 flat_seq = full_seq.reshape(full_seq.shape[0], -1)  # [5, T]
 prompt_list = flat_seq[:4]  
 query_T = flat_seq[4]     
-
 input_ids = np.concatenate(prompt_list).astype(np.int64)
 input_ids = torch.tensor(input_ids, dtype=torch.long).unsqueeze(0)
-
 print(f"Total prompt token length: {input_ids.shape[1]}")
 
 max_new = query_T.shape[0]  
@@ -39,4 +37,3 @@ H = W = int(np.sqrt(max_new))
 gen_map = generated.reshape(H, W)
 np.save(save_path, gen_map)
 print(f"Prediction saved to: {save_path}")
-
