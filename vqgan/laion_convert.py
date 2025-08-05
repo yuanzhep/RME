b59073495bd424881871a7626ee12392721642ea
yuanzhep <93398024+yuanzhep@users.noreply.github.com>
2025-08-05 15:49:02 -0400

    Update

--- vqgan/laion_convert.py
+++ vqgan/laion_convert.py
@@ -7,45 +7,35 @@ import subprocess
 import tarfile
 import time
 from multiprocessing import Pool
-
 import numpy as np
 import torch
 import torch.nn as nn
 from timm.data import ImageDataset
 from torch.utils.data import DataLoader
-
-torch.set_grad_enabled(False)
-random.seed(42)
-
 from load import encode_transform, load_model
 
-# set seed
-random_seed = 1
+torch.set_grad_enabled(False)
+# random.seed(42)
+random_seed = 42
 torch.manual_seed(random_seed)
 torch.cuda.manual_seed(random_seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 np.random.seed(random_seed)
 
-# args
 parser = argparse.ArgumentParser()
-# unzip
-parser.add_argument("--folder_path", type=str, default='/cache/data/laion400m-images/part0/')
-parser.add_argument("--extract_path", type=str, default='/home/ma-user/work/laion400m-images/part0_jpg/')
+parser.add_argument("--folder_path", type=str, default='.../data/laion400m-images/part0/')
+parser.add_argument("--extract_path", type=str, default='.../laion400m-images/part0_jpg/')
 parser.add_argument("--prefix", type=str, default='laion_part0')
 parser.add_argument("--num_processes", type=int, default=10)
-# vqgan convert
-parser.add_argument("--data", type=str, default='/cache/laion_jpg/part0/')  # folder of unziped imgs
+parser.add_argument("--data", type=str, default='.../cache/laion_jpg/part0/')  # folder of unziped imgs
 parser.add_argument("--batch_size", type=str, default=256)  # folder of imgs
 parser.add_argument("--type", type=str, default="internlm")
-parser.add_argument("--output", type=str, default="/cache/laion_train_convert/part0/")
-parser.add_argument("--model_name_or_path", type=str, default="/cache/ckpt/vqgan-f16-8192-laion")
+parser.add_argument("--output", type=str, default=".../cache/laion_train_convert/part0/")
+parser.add_argument("--model_name_or_path", type=str, default=".../cache/ckpt/vqgan-f16-8192-laion")
 args = parser.parse_args()
 args.data = args.extract_path
-
-# unzip part
 unzip_start_time = time.time()
-
 
 def extract_tar(tar_info):
     tar_file, folder_path, extract_path, prefix = tar_info
@@ -56,14 +46,12 @@ def extract_tar(tar_info):
         tar.extractall(target_folder)
     
     print(f"Extracted {tar_file} to {target_folder}")
-    
     cmd_txt = 'yes | rm -r ' + target_folder + '/*.txt'
     subprocess.run(cmd_txt, shell=True)
     cmd_json = 'yes | rm -r ' + target_folder + '/*.json'
     subprocess.run(cmd_json, shell=True)
     cmd_tar_file = 'yes | rm -r ' + tar_file_path
     subprocess.run(cmd_tar_file, shell=True)
-
 
 def extract_all_tarfiles_parallel(folder_path, extract_path, prefix, num_processes=4):
     file_list = [file for file in os.listdir(folder_path) if file.endswith('.tar')]
@@ -78,12 +66,9 @@ print('########### unzip time: ', time.time() - unzip_start_time)
 
 convert_start_time = time.time()
 
-
-# convert part
 def list_subdir(folder_path):
     subdir = [f.name for f in os.scandir(folder_path) if f.is_dir()]
     return subdir
-
 
 dir_names = list_subdir(args.data)
 
@@ -94,7 +79,6 @@ print(len(dir_names))
 vq_model = load_model(args.model_name_or_path)
 vq_model = vq_model.cuda().eval()
 
-
 class ParallelWrapper(nn.Module):
     def __init__(self, vq_model, func='encode'):
         super(ParallelWrapper, self).__init__()
@@ -104,10 +88,8 @@ class ParallelWrapper(nn.Module):
     def forward(self, x):
         return getattr(self.vq_model, self.func)(x)
 
-
 encoder = ParallelWrapper(vq_model)
 encoder = nn.DataParallel(encoder)
-
 
 def dumps(data):
     seqlen = len(data)
