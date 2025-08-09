62d1c56d6c1e472960fb6d748a6e523d4966403f
yuanzhep <93398024+yuanzhep@users.noreply.github.com>
2025-08-09 16:22:58 -0400

    vqgan.py

--- taming/models/vqgan.py
+++ taming/models/vqgan.py
@@ -1,9 +1,7 @@
 import torch
 import torch.nn.functional as F
 import pytorch_lightning as pl
-
 # from main import instantiate_from_config
-
 from taming.modules.diffusionmodules.model import Encoder, Decoder
 from taming.modules.vqvae.quantize import VectorQuantizer2 as VectorQuantizer
 from taming.modules.vqvae.quantize import GumbelQuantize
