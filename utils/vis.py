67dfe65641644d54e85f945809490d6be2ba5ae7
yuanzhep <93398024+yuanzhep@users.noreply.github.com>
2025-08-09 17:22:19 -0400

    Update

--- utils/vis.py
+++ utils/vis.py
@@ -7,15 +7,11 @@ def save_prediction_map(tensor_map, save_path):
         tensor_map = np.mean(tensor_map, axis=0)  # Convert to [H, W]
     elif tensor_map.ndim == 3 and tensor_map.shape[2] == 3:
         tensor_map = np.mean(tensor_map, axis=2)
-
     tensor_map -= tensor_map.min()
     tensor_map /= (tensor_map.max() + 1e-8)
-
     viridis = plt.cm.get_cmap('viridis')
     colored_map = viridis(tensor_map)[:, :, :3]  
-
     img_uint8 = (colored_map * 255).astype(np.uint8)
-
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
     plt.imsave(save_path, img_uint8)
-    print(f"[Saved] Prediction image to {save_path}")
+    print(f"Prediction image to {save_path}")
